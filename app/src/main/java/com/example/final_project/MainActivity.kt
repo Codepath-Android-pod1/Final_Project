bec622fc37dcced50859506fefceeb560f87d3dc
SciencePotato <houchi6mail@gmail.com>
2022-04-15 22:42:02 -0400

    Added fragments to Main

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.final_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.final_project.fragments.ChatFragment
+import com.example.final_project.fragments.EditFragment
+import com.example.final_project.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.ParseObject
 
 
@@ -20,6 +26,27 @@ class MainActivity : AppCompatActivity() {
                 Log.d("MainActivity","Object saved.")
             }
         }
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener { item ->
+            var fragmentToshow: Fragment? = null
+            when(item.itemId) {
+                R.id.action_Home -> {
+                    fragmentToshow = HomeFragment()
+                }
+                R.id.action_Edit -> {
+                    fragmentToshow = EditFragment()
+                }
+                R.id.action_Chat -> {
+                    fragmentToshow = ChatFragment()
+                }
+            }
+            if(fragmentToshow != null) {
+                fragmentManager.beginTransaction().replace(R.id.FragmentContainer, fragmentToshow).commit()
+            }
+            true
+        }
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_Home
     }
 
     companion object {
