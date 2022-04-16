5649dc5c92111bc4c85a0588e6b3588e40fa33cd
SciencePotato <houchi6mail@gmail.com>
2022-04-15 20:38:17 -0400

    Removed Useless stuff

--- app/src/main/java/com/example/final_project/SignUpActivity.kt
+++ app/src/main/java/com/example/final_project/SignUpActivity.kt
@@ -1,13 +1,8 @@
 package com.example.final_project
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.final_project.databinding.ActivitySignUpBinding
+
 
 class SignUpActivity : AppCompatActivity() {
 
