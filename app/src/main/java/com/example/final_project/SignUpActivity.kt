fb08cc75b55c4b8f7580ef59daafd0a3ba7a608e
SciencePotato <houchi6mail@gmail.com>
2022-04-15 22:10:54 -0400

    Set up Signup Activity

--- app/src/main/java/com/example/final_project/SignUpActivity.kt
+++ app/src/main/java/com/example/final_project/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.example.final_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -13,16 +16,53 @@ class SignUpActivity : AppCompatActivity() {
             setContentView(R.layout.activity_sign_up)
 
         findViewById<Button>(R.id.Signup_SignUp).setOnClickListener {
-
+            val username = findViewById<EditText>(R.id.Signup_Username).text.toString()
+            val password = findViewById<EditText>(R.id.Signup_Password).text.toString()
+            signUpUser(username, password)
+            loginUser(username, password)
+            goToMain()
         }
 
         findViewById<Button>(R.id.Signup_Back).setOnClickListener {
-
+            goToLogin()
         }
      }
 
-    private fun goToSignUp() {
-        val intent = Intent(this, SignUpActivity::class.java)
+    private fun signUpUser(username: String, password: String){
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // successfully signed in
+                goToMainActivity()
+                Toast.makeText(this, "Successfully Created", Toast.LENGTH_SHORT).show()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(LoginActivity.TAG, "Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
+
+
+    private fun goToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
