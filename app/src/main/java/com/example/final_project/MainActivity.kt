c8a713d8bcb26d7bca4819d4282e273db3c16633
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-04-16 14:35:59 -0400

    Commented FirstClass Object Out

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -24,16 +24,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val firstObject = ParseObject("FirstClass")
-        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-        firstObject.saveInBackground {
-            if (it != null){
-                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-            }else{
-                Log.d("MainActivity","Object saved.")
+        /*
+            val firstObject = ParseObject("FirstClass")
+            firstObject.put("message","Hey ! First message from android. Parse is now connected")
+            firstObject.saveInBackground {
+                if (it != null){
+                    it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+                }else{
+                    Log.d("MainActivity","Object saved.")
+                }
             }
-        }
+        */
 
         // Fragments / Bottom Navigation Bar
         val fragmentManager: FragmentManager = supportFragmentManager
