24d39d4960d1826a3d868a878e688cd659055734
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-04-16 14:39:05 -0400

    Add BuildConfig

--- app/src/main/java/com/example/final_project/models/ApiInterface.kt
+++ app/src/main/java/com/example/final_project/models/ApiInterface.kt
@@ -4,13 +4,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// https://stackoverflow.com/questions/70556055/how-to-convert-geohash-to-latitude-and-longitude-in-kotlin
 interface ApiInterface {
     @GET("/discovery/v2/events")
-    fun getEvents(): Call<Events>
-
-    @GET("/discovery/v2/events")
-    fun getEventsbyLocation(@Query("latlong") latlong: String): Call<Events>
-
-    @GET("/discovery/v2/events")
-    fun getEventsbyCity(@Query("city") city: String): Call<Events>
+    fun getEvents(
+        @Query("city") city: String,
+        @Query("keyword") keyword: String,
+        @Query("geoPoint") geoHash: String,
+    ): Call<Events>
 }
