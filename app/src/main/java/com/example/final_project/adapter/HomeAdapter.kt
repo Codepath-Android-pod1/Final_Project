16be3be505e2abe09caabe3949843df958836aaa
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-04-20 17:06:35 -0400

    TabLayout On fragment_home.xml Done

---
+++ app/src/main/java/com/example/final_project/adapter/HomeAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.final_project.adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.ReportFragment
+import com.example.final_project.fragments.ChatFragment
+import com.example.final_project.fragments.HomeFragment
+import com.example.final_project.fragments.SettingFragment
+
+@Suppress("DEPRECATION")
+internal class MyAdapter(
+    var context: HomeFragment,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                ChatFragment()
+            }
+            1 -> {
+                SettingFragment()
+            }
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
--- app/src/main/java/com/example/final_project/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/final_project/fragments/HomeFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
 import com.example.final_project.R
+import com.example.final_project.adapter.MyAdapter
+import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : Fragment() {
 
-    lateinit var toolbar: Toolbar
+    lateinit var vp: ViewPager
+    lateinit var tl: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,28 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vp = view.findViewById<ViewPager>(R.id.viewpager)
+        tl = view.findViewById<TabLayout>(R.id.tablayout)
+
+        tl.setupWithViewPager(vp)
+        tl.addTab(tl.newTab().setText("Testing 1"))
+        tl.addTab(tl.newTab().setText("Testing 2"))
+        tl.tabGravity = TabLayout.GRAVITY_FILL
+        val adapter = MyAdapter(this, parentFragmentManager, tl.tabCount)
+        vp.adapter = adapter
+        vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tl))
+        tl.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                vp.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 
 }
