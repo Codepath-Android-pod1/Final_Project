0871c2a85e1a1514e981102fbd8c29b26e0114d6
0GhOsTO <cho.yoonho023@gmail.com>
2022-04-23 18:38:21 -0400

    Moving to Profile Fragment.

--- app/src/main/java/com/example/final_project/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/final_project/fragments/ProfileFragment.kt
@@ -2,12 +2,16 @@ package com.example.final_project.fragments
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.final_project.R
+import com.example.final_project.activities.ProfileActivity
 import com.example.final_project.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -25,6 +29,13 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.ProfileSaveButton.setOnClickListener { view ->
+            Log.i(ProfileActivity.TAG, "시발 버튼 눌렸다고 썅")
+            val name = getView()?.findViewById<EditText>(R.id.Profile_Name).text.toString()
+            val email = getView()?.findViewById<EditText>(R.id.Profile_Email).text.toString()
+            val phone = getView()?.findViewById<EditText>(R.id.Profile_PhoneNum).text.toString()
+            //updateUser(name, email, phone)
+        }
         binding.tvDeleteAccount.setOnClickListener{
             Toast.makeText(requireContext(), "Needs to confirm a few things...", Toast.LENGTH_SHORT)
                 .show()
