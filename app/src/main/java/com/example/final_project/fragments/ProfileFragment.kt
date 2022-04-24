8914ed12cea72f057fd965e6430642095d05c7c8
0GhOsTO <cho.yoonho023@gmail.com>
2022-04-24 00:07:05 -0400

    Profile Fragment Major modified.

--- app/src/main/java/com/example/final_project/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/final_project/fragments/ProfileFragment.kt
@@ -1,16 +1,19 @@
 package com.example.final_project.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.final_project.R
 import com.example.final_project.activities.ProfileActivity
 import com.example.final_project.databinding.FragmentProfileBinding
+import com.parse.*
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -27,16 +30,45 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.ProfileSaveButton.setOnClickListener { view ->
-            Log.i(ProfileActivity.TAG, "시발 버튼 눌렸다고 썅")
-            val name = requireView().findViewById<EditText>(R.id.Profile_Name).text.toString()
-            val email = requireView().findViewById<EditText>(R.id.Profile_Email).text.toString()
-            val phone = requireView().findViewById<EditText>(R.id.Profile_PhoneNum).text.toString()
-            //updateUser(name, email, phone)
+        binding.ProfileSaveButton.setOnClickListener {
+            Log.i(ProfileActivity.TAG, "Tlqkf 버튼 눌렸다고")
+            val name = getView()?.findViewById<EditText>(R.id.Profile_Name)?.text.toString()
+            val email = getView()?.findViewById<EditText>(R.id.Profile_Email)?.text.toString()
+            val phone = getView()?.findViewById<EditText>(R.id.Profile_PhoneNum)?.text.toString()
+            Log.i(ProfileActivity.TAG, ""+name + "/" + email+"/" + phone)
+            updateUser(name, email, phone)
         }
-        binding.tvDeleteAccount.setOnClickListener {
+
+        binding.tvDeleteAccount.setOnClickListener{
             Toast.makeText(requireContext(), "Needs to confirm a few things...", Toast.LENGTH_SHORT)
                 .show()
         }
+    }
+
+    private fun updateUser(name: String, email: String, phone: String) {
+        // Create the ParseUser
+        val user = ParseUser.getCurrentUser()
+        Log.i(ProfileActivity.TAG, ""+user)
+        // Set fields for the user to be created
+        if (name == "" || email == "" || phone == "") {
+            Toast.makeText(context, "Please Enter Name, Email, and Phone Number", Toast.LENGTH_SHORT).show()
+        } else {
+            val params = HashMap<String, Any>()
+            params["objectId"] = user.getObjectId()
+            params["newEmail"] = email
+            params["newPhonenum"] = phone
+            params["newName"] = name
+            Log.i(ProfileActivity.TAG, "ObjectId --> "+user.getObjectId())
+            Log.i(ProfileActivity.TAG, ""+params)
+            ParseCloud.callFunctionInBackground("editUserProperty", params,
+                FunctionCallback{ obj: ParseObject, e: ParseException? ->
+                    if (e == null) {
+                        Log.d("result", obj["email"].toString() + obj["name"].toString() + obj["phonenum"].toString() + " ")
+                    } else {
+                        Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
+
     }
 }
