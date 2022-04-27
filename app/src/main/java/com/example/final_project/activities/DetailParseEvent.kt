f921d3887999d0b9ec094c3e1b3f89766f170627
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-04-27 17:38:22 -0400

    Design activity_detail_event.xml

--- app/src/main/java/com/example/final_project/activities/DetailParseEvent.kt
+++ app/src/main/java/com/example/final_project/activities/DetailParseEvent.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.final_project.R
 import com.example.final_project.adapters.ParseEventAdapter.Companion.EVENT_EXTRA
 import com.example.final_project.models.ParseEvent
@@ -22,5 +23,7 @@ class DetailParseEvent : AppCompatActivity() {
         val event = intent.getParcelableExtra<ParseEvent>(EVENT_EXTRA) as ParseEvent
         tvTitle.text = event.getTitle()
         tvDetails.text = event.getDescription()
+        ivImage = findViewById(R.id.ivImage)
+        ivImage.isVisible = false
     }
 }
